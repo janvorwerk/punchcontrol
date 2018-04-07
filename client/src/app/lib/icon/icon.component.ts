262d9c25424f46da1552c13c96559c66911ce2e1
Jan Vorwerk <jan.vorwerk@gmail.com>
2018-04-07 23:31:45 +0200

    Fixed theming for selected nav icons border color

--- client/src/app/lib/icon/icon.component.ts
+++ client/src/app/lib/icon/icon.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, AfterViewInit } from '@angular/core';
 import { ThemeService } from '../theme.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/delay';
+import { LOGGING } from '../../util/logging';
+import { Router } from '@angular/router';
+
+const LOGGER = LOGGING.getLogger('IconComponent');
 
 @Component({
     selector: 'app-icon',
@@ -10,9 +14,8 @@ import 'rxjs/add/operator/delay';
     styleUrls: ['./icon.component.scss']
 })
 export class IconComponent implements OnInit {
-
     fill: Observable<string>;
-    constructor(themeService: ThemeService, private element: ElementRef) {
+    constructor(themeService: ThemeService, private element: ElementRef, private router: Router) {
         this.fill = themeService.theme.delay(100).map(() => window.getComputedStyle(element.nativeElement.querySelector('svg')).color);
     }
 
@@ -23,5 +26,11 @@ export class IconComponent implements OnInit {
     icon = '';
 
     ngOnInit() {
+    }
+
+    state(route: string|[string, any]) {
+        const path = route instanceof Array ? route[0] : route;
+        const active = this.router.isActive(path, false);
+        return active ? 'active' : '';
     }
 }
--- client/src/app/lib/nav/nav.component.ts
+++ client/src/app/lib/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { RacesService } from '../../data/races.service';
 
 @Component({
@@ -8,8 +8,17 @@ import { RacesService } from '../../data/races.service';
 })
 export class NavComponent implements OnInit {
     selectedRaceId: number;
+    top = [
+        {link: '/register', icon: 'register', title: 'register a runner'},
+        {link: ['/runner', {race: this.selectedRaceId}], icon: 'runner', title: 'Runners'},
+        {link: ['/teams', {race: this.selectedRaceId}], icon: 'team', title: 'Teams'},
+        {link: ['/race', {race: this.selectedRaceId}], icon: 'race', title: 'Race setup'},
+        {link: ['/starttime', {race: this.selectedRaceId}], icon: 'time', title: 'Start times'},
+        {link: ['/listings', {race: this.selectedRaceId}], icon: 'listing', title: 'Listings'},
+        {link: ['/statistics', {race: this.selectedRaceId}], icon: 'statistics', title: 'Statistics'},
+        {link: '/admin', icon: 'admin', title: 'Admin'},
+    ];
     constructor(private racesService: RacesService) { }
-
     ngOnInit() {
         this.racesService.races.subscribe(state => this.selectedRaceId = state.selectedRaceId);
     }
