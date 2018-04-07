44327cd1a3c40dc8284e9603d95601d8ba396c3c
Jan Vorwerk <jan.vorwerk@gmail.com>
2018-04-07 17:27:19 +0200

    Persist selected theme in localStorage

--- client/src/app/lib/theme.service.ts
+++ client/src/app/lib/theme.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { LOGGING } from '../util/logging';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
 const LOGGER = LOGGING.getLogger('ThemeService');
 
 const THEMES = {
@@ -15,14 +16,29 @@ export class ThemeService {
 
     themes = Object.keys(THEMES) as Theme[];
 
-    private _currentTheme = new BehaviorSubject<Theme>(this.themes[0]);
-    theme = this._currentTheme.asObservable();
+    private _currentTheme: BehaviorSubject<Theme>;
+    theme: Observable<Theme>;
 
-    constructor() { }
+    constructor() {
+        let themeStr = localStorage.getItem('theme');
+        const allowed = this.themes as string[];
+        if (!allowed.includes(themeStr)) {
+            themeStr = this.themes[0];
+        }
+        const theme = themeStr as Theme;
+        this._currentTheme = new BehaviorSubject<Theme>(theme);
+        this.updateHref(theme);
+        this.theme = this._currentTheme.asObservable();
+    }
 
     setTheme(theme: Theme) {
         this._currentTheme.next(theme);
         LOGGER.info(`Switching theme to ${theme}`);
+        localStorage.setItem('theme', theme);
+        this.updateHref(theme);
+    }
+
+    private updateHref(theme: Theme) {
         const themeSelector = document.querySelector('head > link.theme-selector-link');
         (themeSelector as any).href = `assets/gen/theme-${theme}.css`;
     }
